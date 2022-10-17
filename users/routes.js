cc46eb90f4174b5a5798e6c3282af44cd6ec1cb7
jpdealba <jpdeaty1207@gmail.com>
2022-10-17 17:28:14 -0500

    routes updated

--- users/routes.js
+++ users/routes.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-// Obtiene todos los usuarios
+// Obtiene el usuario
 router.get("/:id", (req, res) => {
   res.send("Get user works with id: " + req.params.id).status(200);
 });
 
-// Crea un nuevo usuario
+// Crea un nuevo usuario -. {name, email, photo, password}
 router.post("/", (req, res) => {
   res.send("Post new user works").status(201);
 });
 
-// Actualiza un usuario
-router.put("/", (req, res) => {
-  res.send("Put update user image works").status(201);
+// Actualiza un usuario - Actualiza un nuevo usuario - {name?, email?, photo?, password?}
+router.put("/:user_id", (req, res) => {
+  res
+    .send("Put update user works with user_id: " + req.params.user_id)
+    .status(201);
 });
 
 module.exports = router;
