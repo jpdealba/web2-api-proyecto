9b612e9eb05b289ca6d664ad85ef0c3b241421f5
Pablo <nunezp20@gmail.com>
2022-11-09 17:51:16 -0600

    first push

--- src/users/routes.js
+++ src/users/routes.js
@@ -1,10 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./controller");
+
 // Obtiene el usuario
+/**
+ * @swagger
+ * /api/users/:id:
+ *  get:
+ *    description: Obtiene el usuario según su id
+ *    parameters:
+ *      - in: query|path
+ *        name: id
+ *        description: put id
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Obtiene el usuario
+ *      404:
+ *        description: Not found
+ */
 router.get("/:id", controller.getOne);
 
 // Crea un nuevo usuario -. {name, email, photo, password}
+/**
+ * @swagger
+ * /api/users/:id:
+ *  post:
+ *    description: Obtiene el usuario según su id
+ *    parameters:
+ *      - in: query|path
+ *        name: id
+ *        description: put id
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Obtiene el usuario
+ *      404:
+ *        description: Not found
+ */
 router.post("/", controller.postOne);
 
 // Actualiza un usuario - Actualiza un nuevo usuario - {name?, email?, photo?, password?}
