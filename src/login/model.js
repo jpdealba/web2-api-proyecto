8e6d14a7497e8c94b57a2cba835198d6c5c0979f
jpdealba <jpdeaty1207@gmail.com>
2022-11-21 23:19:18 -0600

    test

--- src/login/model.js
+++ src/login/model.js
@@ -5,48 +5,48 @@ class Login {
   async createOne(data) {
     const db = database();
     const collection = db.collection("users");
-    // try {
-    //   const resp = await collection.findOne({
-    //     email: data.email,
-    //     password: data.password,
-    //   });
-    //   if (resp) {
-    //     return {
-    //       token: jwt.sign(
-    //         { email: data.email, password: data.password },
-    //         process.env.TOKEN_SECRET
-    //       ),
-    //     };
-    //   } else {
-    //     return null;
-    //   }
-    // } catch (err) {
-    //   return null;
-    // }
+    try {
+      const resp = await collection.findOne({
+        email: data.email,
+        password: data.password,
+      });
+      if (resp) {
+        return {
+          token: jwt.sign(
+            { email: data.email, password: data.password },
+            process.env.TOKEN_SECRET
+          ),
+        };
+      } else {
+        return null;
+      }
+    } catch (err) {
+      return null;
+    }
   }
 
   async verifyOne(token) {
-    //   if (!token) return false;
+    if (!token) return false;
     const db = database();
     const collection = db.collection("users");
-    //   try {
-    //     // const db = database();
-    //     // const collection = db.collection("users");
-    //     const decoded = jwt.decode(token, process.env.TOKEN_SECRET);
-    //     const resp = await collection.findOne({
-    //       email: decoded.email,
-    //       password: decoded.password,
-    //     });
-    //     return jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    //       if (err) {
-    //         return false;
-    //       } else {
-    //         return { ...resp, token: token };
-    //       }
-    //     });
-    //   } catch (err) {
-    //     return false;
-    //   }
+    try {
+      // const db = database();
+      // const collection = db.collection("users");
+      const decoded = jwt.decode(token, process.env.TOKEN_SECRET);
+      const resp = await collection.findOne({
+        email: decoded.email,
+        password: decoded.password,
+      });
+      return jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        if (err) {
+          return false;
+        } else {
+          return { ...resp, token: token };
+        }
+      });
+    } catch (err) {
+      return false;
+    }
   }
 }
 
