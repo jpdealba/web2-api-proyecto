5aca47f0d325fcd56361c51448c1e7eca5203eb2
jpdealba <jpdeaty1207@gmail.com>
2022-12-03 08:11:24 -0600

    test

--- src/coins/model.js
+++ src/coins/model.js
@@ -26,70 +26,68 @@ class Coin {
     }
   }
 
-  async updateDB() {
+  updateDB() {
     try {
       dbd
         .connect()
-        .then(async (client) => {
+        .then((client) => {
           const db = client.db("CoinCap");
           database(db);
-          const test = await axios.get(
-            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&sparkline=false"
-          );
-          console.log(test.data);
-          console.log(typeof test.data);
-          // .then((res) => {
-          //   try {
-          //     const collection = db.collection("coins");
-          //     if (res.data && res.data.length > 0) {
-          //       const bulk = collection.initializeUnorderedBulkOp();
-          //       console.log(typeof res.data);
-
-          //   JSON.parse(res.data).map((coin) => {
-          //     bulk
-          //       .find({ id: coin.id })
-          //       .upsert()
-          //       .updateOne({
-          //         $set: {
-          //           id: coin.id,
-          //           symbol: coin.symbol,
-          //           name: coin.name,
-          //           image: coin.image,
-          //           current_price: coin.current_price,
-          //           market_cap: coin.market_cap,
-          //           market_cap_rank: coin.market_cap_rank,
-          //           fully_diluted_valuation: coin.fully_diluted_valuation,
-          //           total_volume: coin.total_volume,
-          //           high_24h: coin.high_24h,
-          //           low_24h: coin.low_24h,
-          //           price_change_24h: coin.price_change_24h,
-          //           price_change_percentage_24h:
-          //             coin.price_change_percentage_24h,
-          //           market_cap_change_24h: coin.market_cap_change_24h,
-          //           market_cap_change_percentage_24h:
-          //             coin.market_cap_change_percentage_24h,
-          //           circulating_supply: coin.circulating_supply,
-          //           total_supply: coin.total_supply,
-          //           max_supply: coin.imax_supplyd,
-          //           ath: coin.ath,
-          //           ath_change_percentage: coin.ath_change_percentage,
-          //           ath_date: coin.ath_date,
-          //           atl: coin.atl,
-          //           atl_change_percentage: coin.atl_change_percentage,
-          //           atl_date: coin.atl_date,
-          //           roi: coin.roi,
-          //           last_updated: coin.last_updated,
-          //         },
-          //       });
-          //   });
-          //   bulk.execute(function (err, updateResult) {
-          //     console.log(err, updateResult);
-          //   });
-          // }
-          // } catch (err) {
-          //   console.log(err);
-          // }
-          // });
+          axios
+            .get(
+              "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&sparkline=false"
+            )
+            .then((res) => {
+              try {
+                const collection = db.collection("coins");
+                if (res.data && res.data.length > 0) {
+                  const bulk = collection.initializeUnorderedBulkOp();
+                  console.log(typeof JSON.stringify(res.data));
+                  JSON.stringify(res.data).map((coin) => {
+                    bulk
+                      .find({ id: coin.id })
+                      .upsert()
+                      .updateOne({
+                        $set: {
+                          id: coin.id,
+                          symbol: coin.symbol,
+                          name: coin.name,
+                          image: coin.image,
+                          current_price: coin.current_price,
+                          market_cap: coin.market_cap,
+                          market_cap_rank: coin.market_cap_rank,
+                          fully_diluted_valuation: coin.fully_diluted_valuation,
+                          total_volume: coin.total_volume,
+                          high_24h: coin.high_24h,
+                          low_24h: coin.low_24h,
+                          price_change_24h: coin.price_change_24h,
+                          price_change_percentage_24h:
+                            coin.price_change_percentage_24h,
+                          market_cap_change_24h: coin.market_cap_change_24h,
+                          market_cap_change_percentage_24h:
+                            coin.market_cap_change_percentage_24h,
+                          circulating_supply: coin.circulating_supply,
+                          total_supply: coin.total_supply,
+                          max_supply: coin.imax_supplyd,
+                          ath: coin.ath,
+                          ath_change_percentage: coin.ath_change_percentage,
+                          ath_date: coin.ath_date,
+                          atl: coin.atl,
+                          atl_change_percentage: coin.atl_change_percentage,
+                          atl_date: coin.atl_date,
+                          roi: coin.roi,
+                          last_updated: coin.last_updated,
+                        },
+                      });
+                  });
+                  bulk.execute(function (err, updateResult) {
+                    console.log(err, updateResult);
+                  });
+                }
+              } catch (err) {
+                console.log(err);
+              }
+            });
         })
         .catch((err) => console.log(err))
         .catch((err) => {
